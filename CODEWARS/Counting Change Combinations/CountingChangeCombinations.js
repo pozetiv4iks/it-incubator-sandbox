1003234fcb36607fba3224e8498386fc1e6d787e
pozetiv4iks <stepan.ewqazsx@gmail.com>
2024-05-13 13:27:11 +0300

    new: task codewas

---
+++ CODEWARS/Counting Change Combinations/CountingChangeCombinations.js
@@ -0,0 +1,8 @@
+var countChange = function(money, coins) {
+    if(money < 0 || coins.length === 0)
+      return 0
+    else if(money === 0)
+      return 1
+    else
+      return countChange(money - coins[0], coins) + countChange(money, coins.slice(1))
+  }
