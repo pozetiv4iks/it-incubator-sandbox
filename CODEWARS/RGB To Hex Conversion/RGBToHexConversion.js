e3a059d6dec2297177261f2abc7fa2e90c38f4ee
pozetiv4iks <stepan.ewqazsx@gmail.com>
2024-05-13 10:14:45 +0300

    add: task

---
+++ CODEWARS/RGB To Hex Conversion/RGBToHexConversion.js
@@ -0,0 +1,13 @@
+function rgb(r, g, b) {
+    r = Math.max(0, Math.min(255, r));
+    g = Math.max(0, Math.min(255, g));
+    b = Math.max(0, Math.min(255, b));
+    
+    const hexR = r.toString(16).padStart(2, '0');
+    const hexG = g.toString(16).padStart(2, '0');
+    const hexB = b.toString(16).padStart(2, '0');
+    
+    const hexColor = hexR + hexG + hexB;
+    
+    return hexColor.toUpperCase();
+  }
