bd17205bd120b2bc963d29365e3513d108dd3a47
pozetiv4iks <stepan.ewqazsx@gmail.com>
2024-05-15 18:47:35 +0300

    codewars task

---
+++ CODEWARS/Minimize Sum Of Array (Array Series #1)/script.js
@@ -0,0 +1,8 @@
+function minSum(arr) {
+    const sorted=arr.sort((a,b)=>a-b)
+    let sum=0;
+    for (let i=0;i<arr.length/2;i++){
+    sum+=sorted[i]*sorted[sorted.length-1-i]
+    }
+    return sum
+   }
