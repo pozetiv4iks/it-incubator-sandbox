fd5e3ef6aa563c0bf301687f9be3db6c883ad301
pozetiv4iks <stepan.ewqazsx@gmail.com>
2024-05-23 17:31:43 +0300

    add codewars

---
+++ CODEWARS/Mexican Wave/script.js
@@ -0,0 +1,10 @@
+function wave(str){
+    let result = [];
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] !== ' ') {
+        let wave = str.slice(0, i) + str[i].toUpperCase() + str.slice(i + 1);
+        result.push(wave);
+      }
+    }
+    return result;
+  }
