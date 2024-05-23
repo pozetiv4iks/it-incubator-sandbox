3cac1a97a3298915065873eeb891fa88ef471091
pozetiv4iks <stepan.ewqazsx@gmail.com>
2024-05-23 17:55:41 +0300

    add codewars

---
+++ CODEWARS/Mexican Wave/second.js
@@ -0,0 +1,11 @@
+function wave(str){
+    let result = [];
+    
+    str.split("").forEach((char, index) => {
+        if (/[a-z]/.test(char)) {
+            result.push(str.slice(0, index) + char.toUpperCase() + str.slice(index + 1));
+        }
+    });
+    
+    return result;
+}
